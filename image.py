35d9b6a52c135984a95f1c7ac2d4b73ee70bf761
Sebastian Rochacki <sebastian.rochacki@gmail.com>
2020-02-11 19:30:56 +0100

    exceptions in image.py

--- image.py
+++ image.py
@@ -7,9 +7,7 @@ except:
     print('Program requires ' + lib_1 + ' python module to be installed')
     print('Recommended' + lib_1 + '2.0.0')
     exit(1)
-
-from tensorflow import keras        # open-source neural-network library, set of images
-
+from tensorflow import keras            # open-source neural-network library, set of images
 try:
     import numpy as np                  # adding support for large, multi-dimensional arrays and matrices
     print("Numpy " + np.__version__)
@@ -18,26 +16,31 @@ except:
     print('Program requires ' + lib_2 + ' python module to be installed')
     print('Recommended' + lib_2 + '1.17.4')
     exit(1)
-
 try:
     import matplotlib.pyplot as plt     # Python 2D plotting library
 except:
     lib_3 = 'Matplotlib'
     print('Program requires ' + lib_3 + ' python module to be installed')
     exit(1)
-
 try:
-    from PIL import Image  # Python Imaging Library
+    from PIL import Image                # Python Imaging Library
     print("PIL " + Image.__version__)
 except:
     lib_4 = 'PIL'
     print('Program requires ' + lib_4 + ' python module to be installed')
     print('Recommended' + lib_4 + '6.2.1')
     exit(1)
+import os                                # to change path os.chdir('...')
 
-import os                           # to change path os.chdir('...')
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--outputDir", help="Destination dir", type=str, default="results")
+args = parser.parse_args()
 
-main_folder = os.getcwd()           # path to main folder with image.py
+main_folder = os.getcwd()            # path to main folder with image.py
+rgba = 255                           # max value for color matrix
+shape = 28                           # number of pixels for network image
+max_variation = 0.1                  # max image variation from a square, 0.1 <-> 10%
 
 model = tf.keras.models.load_model('my_model.h5')   # loading the trained model
 fashion_mnist = keras.datasets.fashion_mnist        # training and test dataset
@@ -45,8 +48,8 @@ fashion_mnist = keras.datasets.fashion_mnist        # training and test dataset
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-train_images = train_images/255
-test_images = test_images/255
+train_images = train_images/rgba
+test_images = test_images/rgba
 
 items = {
     0: "T-shirt/Top",
@@ -101,7 +104,7 @@ def compile_model():
 
 def build_model():
     model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(28, 28)),     # transforms the format of the images from a two-dimensional array (of 28 by 28 pixels) to a one-dimensional array (of 28 * 28 = 784 pixels)
+        keras.layers.Flatten(input_shape=(shape, shape)),     # transforms the format of the images from a two-dimensional array (of 28 by 28 pixels) to a one-dimensional array (of 28 * 28 = 784 pixels)
         keras.layers.Dense(128, activation='relu'),     # the first dense layer has 128 nodes
         keras.layers.Dense(10, activation='softmax')    # the second layer is a 10-node softmax layer that returns an array of 10 probability scores that sum to 1
     ])                                                  # Each node contains a score that indicates the probability that the current image belongs to one of the 10 classes.
@@ -116,15 +119,19 @@ def evaluate_model():
 "USER PART"
 print(items)
 print('\n')
-print('There is two option of loading image:')
-print('0. Exit')
-print('1. By give a name of image')
-print('2. Series, by give a amount of images called: picture_i.png')
-print('3. By give a name of image and re - train this photo')
-print('4. Reset')
-mode = input('Enter 0, 1, 2, 3 or 4: ')
-mode = int(mode)
-if mode == 2:
+print('Options of loading image:')
+print('1. By give a name of image, command: \'name\'')
+print('2. By give a name of image and re-train this photo, command: \'name+\'')
+print('3. Series, by give a amount of images called: picture_i.png, command: \'series\'')
+print('4. Reset, command: \'reset\'')
+print('5. Exit, command: \'exit\'')
+while True:     # loading options by user
+    mode = input('Choose option: ')
+    if mode == 'name' or mode == 'series' or mode == 'name+' or mode == 'reset' or mode == 'exit':
+        break
+    else:
+        print('Wrong command option')
+if mode == 'series':
     n = input('How many pictures is in folder pictures: i = ')
     n = int(n)
 
@@ -138,77 +145,81 @@ f = open("results.dat", "w+")
 
 while state:
     name = ''
-    if mode == 0:
-        break
-    if mode == 1:
-        name = input("Give a name of file or 0, if exit: ")
-        if name == '0':
+    if mode == 'name':
+        name = input("Give a \'name\' of file or \'exit\', if exit: ")
+        if name == 'exit':
             break
-    if mode == 2:
+    if mode == 'name+':
+        name = input("Give a \'name\' of file to retrain the program or \'exit\', if exit: ")
+        if name == 'exit':
+            break
+    if mode == 'series':
         ii = ii + 1
         if ii > n:
             break
         name = 'picture_' + str(ii) + '.png'
         print(name)
-    if mode == 3:
-        name = input("Give a name of file to retrain the program or 0, if exit: ")
-        if name == '0':
-            break
-    if mode == 4:   # LOAD ORIGINAL DATA TEST
+    if mode == 'reset':   # LOAD ORIGINAL DATA TEST
         fashion_mnist = keras.datasets.fashion_mnist  # training and test dataset
         (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()  # loading training and test dataset
         class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                        'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-        train_images = train_images/255
-        test_images = test_images/255
+        train_images = train_images/rgba
+        test_images = test_images/rgba
         model = build_model()
         compile_model()
         train_model()
         os.chdir(main_folder)
         model.save('my_model.h5')  # save new model
         break
+    if mode == 'exit':
+        break
     os.chdir(main_folder + '/pictures')     # path to the images
-    img = Image.open(name)
+    try:
+        img = Image.open(name)
+    except:
+        print('Wrong file name')
+        exit(1)
 
-    if img.size[0] != img.size[1]:      # Does the photo have N x X pixels ?
-        x = img.size[0]
-        y = img.size[1]
-        if abs(x-y)/x < 0.1 and abs(x-y)/y < 0.1:       # auto - correction if photo is close to square
-            if x > y:
-                tmp = int((x-y)/2)
-                cropped = img.crop((tmp, 1, y+tmp-1, y))      # crop((left, top, right, bottom))
+    if img.size[0] != img.size[1]:                                       # Does the photo have N x X pixels ?
+        x = img.size[0]                                                  # pixels number of image from OX
+        y = img.size[1]                                                  # pixels number of image from OY
+        if abs(x-y)/x < max_variation and abs(x-y)/y < max_variation:    # auto-correction if photo is close to square
+            if x > y:                                                    # choose longer side
+                tmp = int((x-y)/2)                                       # temporary variable to symmetric image cut
+                cropped = img.crop((tmp, 1, y+tmp-1, y))                 # crop((left, top, right, bottom)), cut for size: y x y
                 cropped.save(name)
                 img = cropped
             if y > x:
                 tmp = int((y-x)/2)
-                cropped = img.crop((1, tmp, x, x+tmp-1))      # crop((left, top, right, bottom))
+                cropped = img.crop((1, tmp, x, x+tmp-1))                 # crop((left, top, right, bottom)), cut for size: X x X
                 cropped.save(name)
                 img = cropped
         else:
             print('The image is not a square (auto-correction for max 10% difference: x-y)')
 
-    img.thumbnail((28, 28), Image.ANTIALIAS)    # resizes image for a neural - network set
+    img.thumbnail((shape, shape), Image.ANTIALIAS)          # resize image for a neural - network set
     img = np.asarray(img)[:, :, 0]
 
-    img = img/255       # color -> black and white
+    img = img/rgba                          # color -> black and white
     i = 0
     j = 0
-    for i in range(28):
-      for j in range(28):
-            img[i, j] = abs(1-img[i, j])
+    for i in range(shape):
+      for j in range(shape):
+            img[i, j] = abs(1-img[i, j])    # color correction -> white background of image
 
-    plot_matrix(img, name)                # save prepared image
-    pred = prediction_imag(img, name)     # prediction for loaded image
+    plot_matrix(img, name)                  # save prepared image
+    pred = prediction_imag(img, name)       # prediction for loaded and prepared image
     print(name + ' with ' + str(round(pred[1]*100, 1)) + '% is a ' + str(items[pred[0]]))
     f.write(name + ' with ' + str(round(pred[1]*100, 1)) + '% is a ' + str(items[pred[0]]) + '\n')
 
-    if pred[1] < 0.5 or mode == 3:
+    if pred[1] < 0.5 or mode == 'name+':
         print('Do you want retrain neural-network for ' + name)
-        choice = input('Enter \'yes\' or anything else, if no: ')
+        choice = input('Enter \'yes\' or anything else if no: ')
         if choice == 'yes':
             print(items)
-            number = input("Put 0,1...9 to point right class: ")
-            tmp1 = np.ones((1, 28, 28))
+            number = input("Put \'0,1...9\' to point right class: ")
+            tmp1 = np.ones((1, shape, shape))
             tmp1[0] = img
             tmp2 = int(number)
             train_images = np.concatenate((train_images, tmp1))
